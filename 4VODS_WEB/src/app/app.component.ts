d64790d20a14d070485b75a7d1ee39744da936f2
Inero-dev <ikeribero6@gmail.com>
2025-02-26 14:00:26 +0100

    Agregado el popup en la lista

--- 4VODS_WEB/src/app/app.component.ts
+++ 4VODS_WEB/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
-import { IniciativeDetailComponent } from './components/iniciative-detail/iniciative-detail.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, IniciativeDetailComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- 4VODS_WEB/src/app/components/iniciative-list/iniciative-list.component.ts
+++ 4VODS_WEB/src/app/components/iniciative-list/iniciative-list.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { IniciativeCardComponent } from "../iniciative-card/iniciative-card.component";
 import { Iniciative } from '../../model/iniciative';
 import { IniciativeService } from '../../services/iniciative.service';
+import { IniciativeDetailComponent } from '../../components/iniciative-detail/iniciative-detail.component';
 
 @Component({
   selector: 'app-iniciative-list',
-  imports: [IniciativeCardComponent],
+  imports: [IniciativeCardComponent, IniciativeDetailComponent],
   templateUrl: './iniciative-list.component.html',
   styleUrl: './iniciative-list.component.scss'
 })
