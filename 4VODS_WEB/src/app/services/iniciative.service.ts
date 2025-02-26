f8b924ebc456fcd5758ef0183bd82ef8c15674c9
javiab10 <javierbasoko@gmail.com>
2025-02-26 09:43:57 +0100

    Iconos ODS

--- 4VODS_WEB/src/app/services/iniciative.service.ts
+++ 4VODS_WEB/src/app/services/iniciative.service.ts
@@ -6,12 +6,12 @@ import { Iniciative } from '../model/iniciative';
 })
 export class IniciativeService {
   iniciativeList: Iniciative [] = [
-    new Iniciative(1, 'Reciclaje loco', 'Actividad para concienciar sobre el reciclaje', new Date, new Date, 30),
-    new Iniciative(2, 'Róbale a tu jefe', 'Redistribución de la riqueza', new Date, new Date, 65.5),
-    new Iniciative(3, 'Comida solidaria', 'Concienciación sobre la reducción del hambre', new Date, new Date, 30),
-    new Iniciative(4, 'Peces vivos', 'Concienciación sobre la desintoxicación de nuestros ríos', new Date, new Date, 30),
-    new Iniciative(5, 'Paneles Solares', 'Energía sostenible y no contaminante', new Date, new Date, 30),
-    new Iniciative(6, 'Repoblación de la Selva del Irati', 'Acción por el clima', new Date, new Date, 30),
+    new Iniciative(1, 'Reciclaje loco', 'Actividad para concienciar sobre el reciclaje', new Date, new Date, 30, 'Charla'),
+    new Iniciative(2, 'Róbale a tu jefe', 'Redistribución de la riqueza', new Date, new Date, 30.5, 'Charla'),
+    new Iniciative(3, 'Comida solidaria', 'Concienciación sobre la reducción del hambre', new Date, new Date, 50, 'Proyecto'),
+    new Iniciative(4, 'Peces vivos', 'Concienciación sobre la desintoxicación de nuestros ríos', new Date, new Date, 65, 'Taller'),
+    new Iniciative(5, 'Paneles Solares', 'Energía sostenible y no contaminante', new Date, new Date, 65, 'Taller'),
+    new Iniciative(6, 'Repoblación de la Selva del Irati', 'Acción por el clima', new Date, new Date, 30, 'Charla'),
   ]
 
   constructor() { }
@@ -20,6 +20,9 @@ export class IniciativeService {
     return this.iniciativeList;
   }
 
+  public deleteIniciative(id: number){
+    
+  }
   
   
 }
