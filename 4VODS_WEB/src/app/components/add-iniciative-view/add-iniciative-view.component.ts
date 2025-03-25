fb3e28732e50a2679fdecd92af72ffeb8f9b1ded
javiab10 <javierbasoko@gmail.com>
2025-03-25 13:28:04 +0100

    Estilo formulario scrollable

--- 4VODS_WEB/src/app/components/add-iniciative-view/add-iniciative-view.component.ts
+++ 4VODS_WEB/src/app/components/add-iniciative-view/add-iniciative-view.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FormAddIniciativeComponent } from '../form-add-iniciative/form-add-iniciative.component';
+import { ConfirmModalComponent } from '../modals/confirm-modal/confirm-modal.component';
 
 @Component({
   selector: 'app-add-iniciative-view',
-  imports: [FormAddIniciativeComponent],
+  imports: [FormAddIniciativeComponent, ConfirmModalComponent],
   templateUrl: './add-iniciative-view.component.html',
   styleUrl: './add-iniciative-view.component.scss'
 })
