9201d3742b9c4a8479b050532a44d38285b3f3c0
InakiSebastian <inakicabrerao@gmail.com>
2025-05-01 20:06:24 +0200

    Arreglando año escolar a clonar

--- 4VODS_WEB/src/app/components/form-create-edit/clone-iniciativa/clone-iniciativa.component.ts
+++ 4VODS_WEB/src/app/components/form-create-edit/clone-iniciativa/clone-iniciativa.component.ts
@@ -10,15 +10,13 @@ import { IniciativeService } from '../../../services/iniciative.service';
 import { ModalService } from '../../../services/modal.service';
 import { LoaderComponent } from '../../loader/loader.component';
 
-
 @Component({
   selector: 'app-clone-iniciativa',
   imports: [FormsModule, CommonModule, LoaderComponent],
   templateUrl: './clone-iniciativa.component.html',
-  styleUrl: './clone-iniciativa.component.scss'
+  styleUrl: './clone-iniciativa.component.scss',
 })
 export class CloneIniciativaComponent {
-
   schoolYear: string = '';
   startDate: Date | null = null;
   endDate: Date | null = null;
@@ -33,57 +31,91 @@ export class CloneIniciativaComponent {
 
   isLoading: boolean = false;
 
-  constructor(private route: ActivatedRoute, private modalService: ModalService, private iniciativeService: IniciativeService, private degreeService: DegreeService, private router: Router) {this.isLoading = true;}
+  constructor(
+    private route: ActivatedRoute,
+    private modalService: ModalService,
+    private iniciativeService: IniciativeService,
+    private degreeService: DegreeService,
+    private router: Router
+  ) {
+    this.isLoading = true;
+  }
 
-  async ngOnInit(){
+  async ngOnInit() {
     this.iniciatives = await this.iniciativeService.getCompliteIniciativas();
-    this.route.paramMap.subscribe(params => {
+    this.route.paramMap.subscribe((params) => {
       const idFromRoute = params.get('id');
       if (idFromRoute) {
         this.selectedIniciativeId = parseInt(idFromRoute);
         this.selectIniciative();
       }
     });
-    this.isLoading = false
+    this.isLoading = false;
   }
 
-  create(){
-
-    if(this.selectedIniciativeId == -1 && this.selectedIniciative == null){
+  create() {
+    if (this.selectedIniciativeId == -1 && this.selectedIniciative == null) {
       alert('Debe seleccionar una iniciativa.');
-      return
+      return;
     }
-    const sanitizedYear: any = this.schoolYear.toString().replace(/[-\s]/g, '');
+    const sanitizedYear: any = this.schoolYear.toString();
 
-    if (isNaN(sanitizedYear) || sanitizedYear <= 0) {
-      alert('El año académico debe ser un número válido convinado con "-" y espacios.');
+    if (
+      isNaN(Number(this.schoolYear.toString().replace(/[-\s]/g, ''))) ||
+      sanitizedYear <= 0
+    ) {
+      alert(
+        'El año académico debe ser un número válido convinado con "-" y espacios.'
+      );
       return;
     }
 
-    if(this.startDate == null){
+    if (this.startDate == null) {
       alert('La fecha de inicio no puede ser nula.');
       return;
     }
 
-    if(this.startDate! > this.endDate!) {
+    if (this.startDate! > this.endDate!) {
       alert('La fecha de inicio no puede ser posterior a la de finalización.');
       return;
     }
-    this.iniciativeService.addCompliteIniciative(new NewIniciative(1, this.selectedIniciative!.name,this.selectedIniciative!.description, this.startDate, this.endDate, this.selectedIniciative!.hours, sanitizedYear, this.selectedIniciative!.ods.map((o)=>o.id), this.selectedIniciative!.type, 1, this.selectedIniciative!.teachers.map((t)=>t.id), this.selectedIniciative!.modules.map((m)=>m.id), this.selectedIniciative!.diffusions.map((d)=>d.idDiffusion), this.selectedIniciative!.goals.map((g)=>g.id), [1]));
+    this.iniciativeService.addCompliteIniciative(
+      new NewIniciative(
+        1,
+        this.selectedIniciative!.name,
+        this.selectedIniciative!.description,
+        this.startDate,
+        this.endDate,
+        this.selectedIniciative!.hours,
+        sanitizedYear,
+        this.selectedIniciative!.ods.map((o) => o.id),
+        this.selectedIniciative!.type,
+        1,
+        this.selectedIniciative!.teachers.map((t) => t.id),
+        this.selectedIniciative!.modules.map((m) => m.id),
+        this.selectedIniciative!.diffusions.map((d) => d.idDiffusion),
+        this.selectedIniciative!.goals.map((g) => g.id),
+        [1]
+      )
+    );
     //TODO: recibir el ide con el que se crea
     this.modalService.openModal('detail', this.selectedIniciative);
     this.modalService.rechargeList();
     this.router.navigate(['/iniciatives']);
   }
 
-  async selectIniciative(){
-    this.selectedIniciative = this.iniciatives.find(iniciative => iniciative.id == this.selectedIniciativeId) || null;
-    const ids: number[] = this.selectedIniciative!.modules.map((m)=> m.idDegree);
-    
-    this.degrees = (await this.degreeService.getDegrees()).filter((degree)=> {
-      return ids.includes(degree.id)
-    })
-    console.log("Ciclos seleccionados",await this.degreeService.getDegrees())
-  }
+  async selectIniciative() {
+    this.selectedIniciative =
+      this.iniciatives.find(
+        (iniciative) => iniciative.id == this.selectedIniciativeId
+      ) || null;
+    const ids: number[] = this.selectedIniciative!.modules.map(
+      (m) => m.idDegree
+    );
 
+    this.degrees = (await this.degreeService.getDegrees()).filter((degree) => {
+      return ids.includes(degree.id);
+    });
+    console.log('Ciclos seleccionados', await this.degreeService.getDegrees());
+  }
 }
